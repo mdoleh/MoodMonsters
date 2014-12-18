8718db702d7a80b8587922514434e3db940d0e93
Mohammad Doleh <dammahom59@aim.com>
2014-12-17 20:59:55 -0500

    Alternate scene for angry to reset

---
+++ Assets/Scripts/Angry/TakeItemActionsMenu.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace AngryScene
+{
+    public class TakeItemActionsMenu : MonoBehaviour
+    {
+
+        GameObject[] GUI;
+        Animator anim;
+        bool GUIon = false;
+        public Animator other;
+        float timer = 0.0f;
+
+        public void Awake()
+        {
+            GUI = GameObject.FindGameObjectsWithTag("GUI");
+            anim = GetComponent<Animator>();
+        }
+
+        void Update()
+        {
+            timer += Time.deltaTime;
+            if (timer > 5.0f) // simulate dialogue
+            {
+                StartUsingIPad();
+                if (anim.GetBool("IsUsingIPad")) StartGUI();
+            }
+        }
+
+        public void StartUsingIPad() {
+            anim.SetBool("IsUsingIPad", true);
+            other.SetTrigger("IsAngry");
+        }
+
+        void StartGUI()
+        {
+            if (GUIon) return; // don't want to execute multiple times
+            GUIon = true;
+            for (int ii = 0; ii < GUI.Length; ++ii)
+            {
+                if (GUI[ii].name == "ActionsCanvas")
+                {
+                    GUI[ii].GetComponent<Canvas>().enabled = true;
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SceneReset.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneReset : MonoBehaviour {
+    public string sceneToLoad;
+    AudioSource audio;
+
+    void Awake() {
+        audio = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Lily") {
+            audio.Play();
+        }
+    }
+
+    void Update() {
+        if (!audio.isPlaying) Application.LoadLevel(sceneToLoad);
+    }
+}
