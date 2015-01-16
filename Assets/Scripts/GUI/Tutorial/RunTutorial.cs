2c173033b11713704fa2acde6695becd21792a85
Mohammad Doleh <dammahom59@aim.com>
2015-01-15 22:03:33 -0500

    Replacing audio instructions

--- Assets/Scripts/GUI/Tutorial/RunTutorial.cs
+++ Assets/Scripts/GUI/Tutorial/RunTutorial.cs
@@ -8,6 +8,7 @@ public class RunTutorial : MonoBehaviour
     private AudioSource helpAudio;
     private AudioSource quitAudio;
     private AudioSource repeatAudio;
+    private AudioSource practiceAudio;
 
     private GameObject practiceDropContainer;
     private GameObject practiceButton;
@@ -20,6 +21,7 @@ public class RunTutorial : MonoBehaviour
     private bool explainingHelpButton = false;
     private bool explainingQuitButton = false;
     private bool explainingRepeatButton = false;
+    private bool buttonPushAudioPlayed = false;
 
     public void Initialize()
     {
@@ -34,6 +36,11 @@ public class RunTutorial : MonoBehaviour
     private void Update()
     {
         if (initialAudioPlayed && !sceneAudio.isPlaying) EnablePracticeUI();
+        if (buttonPushAudioPlayed && !buttonPushAudio.isPlaying)
+        {
+            buttonPushAudioPlayed = false;
+            Utilities.PlayAudio(practiceAudio);
+        }
         if (explainingHelpButton && !helpAudio.isPlaying)
         {
             explainingHelpButton = false;
@@ -67,6 +74,7 @@ public class RunTutorial : MonoBehaviour
     private void InitializeAudio()
     {
         buttonPushAudio = transform.Find("ButtonPush").gameObject.GetComponent<AudioSource>();
+        practiceAudio = GetComponent<AudioSource>();
     }
 
     private void EnablePracticeUI()
@@ -77,6 +85,7 @@ public class RunTutorial : MonoBehaviour
         buttonPush.SetActive(true);
         initCanvas.SetActive(false);
         Utilities.PlayAudio(buttonPushAudio);
+        buttonPushAudioPlayed = true;
     }
 
     public void ExplainHelpUI()
