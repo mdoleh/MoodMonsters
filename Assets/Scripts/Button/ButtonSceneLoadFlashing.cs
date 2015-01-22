7962e0a3b8f33dbf5577ccbe4570a009f19bc9ca
Mohammad Doleh <dammahom59@aim.com>
2015-01-21 19:44:26 -0500

    Looping title screen instructions

--- Assets/Scripts/Button/ButtonSceneLoadFlashing.cs
+++ Assets/Scripts/Button/ButtonSceneLoadFlashing.cs
@@ -5,8 +5,23 @@ public class ButtonSceneLoadFlashing : ButtonSelect {
 
     public string sceneToLoad;
 
+    protected override void Awake()
+    {
+        base.Awake();
+        StartCoroutine(StartAudio());
+    }
+
     protected override void DoubleClickAction()
     {
         Utilities.LoadScene(sceneToLoad);
+    }
+
+    private IEnumerator StartAudio()
+    {
+        while (true)
+        {
+            Utilities.PlayAudio(transform.parent.GetComponent<AudioSource>());
+            yield return new WaitForSeconds(5);
+        }
     }
 }
