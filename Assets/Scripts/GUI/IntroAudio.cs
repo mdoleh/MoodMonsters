beb466e4ba8b10a83e8214771b99f379251e774c
Mohammad Doleh <dammahom59@aim.com>
2015-01-21 20:03:59 -0500

    Looping title screen audio + tweaked tutorial drag

---
+++ Assets/Scripts/GUI/IntroAudio.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IntroAudio : MonoBehaviour
+{
+    public int SecondsBetweenRepeat;
+
+    private void Awake()
+    {
+        StartCoroutine(StartAudio(SecondsBetweenRepeat));
+    }
+
+    IEnumerator StartAudio(int seconds)
+    {
+        while (true)
+        {
+            Utilities.PlayAudio(audio);
+            yield return new WaitForSeconds(seconds);
+        }
+    }
+}
