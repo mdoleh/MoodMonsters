499e3c32d4412755d65dd4c7bc6ee101d575a13a
Mohammad Doleh <dammahom59@aim.com>
2015-02-14 17:01:43 -0500

    Detect correct egg based on last scene completed

--- Assets/Game 1 - Catch eggs/Scripts/EggCollider.cs
+++ Assets/Game 1 - Catch eggs/Scripts/EggCollider.cs
@@ -1,14 +1,18 @@
 using UnityEngine;
 using System.Collections;
+using System.Linq;
 
 public class EggCollider : MonoBehaviour {
 
     PlayerScript myPlayerScript;
+    private string lastSceneCompleted;
+    private const string PREFAB_NAME_BASE = "EggPrefab";
 
     //Automatically run when a scene starts
     void Awake()
     {
         myPlayerScript = transform.parent.GetComponent<PlayerScript>();
+        lastSceneCompleted = Globals.Scenes.CompletedScenes.Last();
     }
 
     //Triggered by Unity's Physics
@@ -16,8 +20,17 @@ public class EggCollider : MonoBehaviour {
     {
         //In this game we don't need to check *what* we hit; it must be the eggs
         GameObject collisionGO = theCollision.gameObject;
+	    var emotion = collisionGO.transform.parent.gameObject.name.Replace(PREFAB_NAME_BASE, "");
+	    emotion = emotion.Replace("(Clone)", "");
+	    if (lastSceneCompleted.Contains(emotion))
+	    {
+	        ++myPlayerScript.theScore;
+	    }
+	    else
+	    {
+            --myPlayerScript.theScore;
+	    }
+	    if (myPlayerScript.theScore < 0) myPlayerScript.theScore = 0;
         Destroy(collisionGO.transform.parent.gameObject);
-
-        myPlayerScript.theScore++;
     }
 }
