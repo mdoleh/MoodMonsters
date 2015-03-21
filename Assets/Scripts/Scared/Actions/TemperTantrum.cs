1b4493a59d030d0b4cd4d29668d3f69c6600b751
Mohammad Doleh <dammahom59@aim.com>
2015-03-21 17:39:49 -0400

    Action scripts for AJ

---
+++ Assets/Scripts/Scared/Actions/TemperTantrum.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace ScaredScene
+{
+    public class TemperTantrum : ActoutAction
+    {
+
+        // Use this for initialization
+        private void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+
+        }
+    }
+}
