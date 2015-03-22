edb33f4fbe4fdf485a1dad9d5d5e017b9facd9a0
Mohammad Doleh <dammahom59@aim.com>
2015-03-22 19:14:17 -0400

    Camera tracks AJ

---
+++ Assets/Scripts/Utilities/CameraFollow.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform mainCamera;
+
+	void Update ()
+	{
+        mainCamera.transform.position = new Vector3(transform.position.x, transform.position.y, mainCamera.position.z);
+	}
+}
