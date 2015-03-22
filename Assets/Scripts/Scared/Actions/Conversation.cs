615acadf6fb02038cc43d2de8a76a1e9e2190235
Mohammad Doleh <dammahom59@aim.com>
2015-03-22 13:21:07 -0400

    Conversation intro

---
+++ Assets/Scripts/Scared/Actions/Conversation.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Conversation : MonoBehaviour
+{
+    public Animator anim;
+    public Animator other;
+
+    public void StartListening()
+    {
+        anim.SetTrigger("Listening");
+        other.SetTrigger("Talking");
+    }
+}
