33a69314ae8073f999bc2494b292cd958c3e0f2e
Mohammad Doleh <dammahom59@aim.com>
2015-03-22 15:43:53 -0400

    Transition between conversation and walking

---
+++ Assets/Scripts/Scared/Actions/WalkForward.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace ScaredScene
+{
+    public class WalkForward : MonoBehaviour
+    {
+        private Animator anim;
+        private Animator other;
+
+        private void Start()
+        {
+            anim = GetComponent<Animator>();
+            other = GameObject.Find("Aj").GetComponent<Animator>();
+        }
+
+        public void StartTurning()
+        {
+            anim.SetTrigger("TurnAround");
+            other.SetTrigger("Idle");
+        }
+
+        public void StartWalking()
+        {
+            transform.Rotate(new Vector3(0f, 180f));
+            anim.SetTrigger("Walking");
+            other.SetTrigger("Walking");
+        }
+    }
+}
+
--- Assets/Scripts/Scared/GUI/Tutorial/ScaredTutorial.cs
+++ Assets/Scripts/Scared/GUI/Tutorial/ScaredTutorial.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using Globals;
+using ScaredScene;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ScaredTutorial : TutorialBase
 {
+    public GameObject scarlet;
+
     protected override void HelpExplanationComplete()
     {
-        
+        scarlet.GetComponent<WalkForward>().StartTurning();
     }
 }
