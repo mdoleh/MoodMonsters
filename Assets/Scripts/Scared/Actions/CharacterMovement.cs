0f19aab8656c3bf1d686cef9ace0d436690e5de4
Mohammad Doleh <dammahom59@aim.com>
2015-03-22 20:35:00 -0400

    Walking animation working with Scarlet

--- Assets/Scripts/Scared/Actions/CharacterMovement.cs
+++ Assets/Scripts/Scared/Actions/CharacterMovement.cs
@@ -6,24 +6,37 @@ namespace ScaredScene
     public class CharacterMovement : MonoBehaviour
     {
         private Animator anim;
-        private Animator other;
+        private Animator otherAnim;
+        private GameObject otherCharacter;
+        private bool isWalking = false;
 
         private void Start()
         {
             anim = GetComponent<Animator>();
-            other = GameObject.Find("Aj").GetComponent<Animator>();
+            otherCharacter = GameObject.Find("Aj");
+            otherAnim = otherCharacter.GetComponent<Animator>();
         }
 
         public void StartTurning()
         {
             anim.SetTrigger("TurnAround");
-            other.SetTrigger("Idle");
+            otherAnim.SetTrigger("Idle");
         }
 
         public void StartWalking()
         {
             anim.SetTrigger("Walking");
-            other.SetTrigger("Walking");
+            otherAnim.SetTrigger("Walking");
+            isWalking = true;
+        }
+
+        private void Update()
+        {
+            if (isWalking)
+            {
+                float move = Time.deltaTime * 0.5f;
+                transform.position = new Vector3(transform.position.x + move, transform.position.y, transform.position.z);
+            }
         }
     }
 }
