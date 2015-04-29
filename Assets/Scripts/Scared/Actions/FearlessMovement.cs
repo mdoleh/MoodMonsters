3243d034e69a7d04c8277546be281c13863e06bb
Mohammad Doleh <dammahom59@aim.com>
2015-04-28 21:23:48 -0400

    Camera adjusts to be behind scarlet

--- Assets/Scripts/Scared/Actions/FearlessMovement.cs
+++ Assets/Scripts/Scared/Actions/FearlessMovement.cs
@@ -4,6 +4,8 @@ using ScaredScene;
 
 public class FearlessMovement : CharacterMovement
 {
+    public Camera mainCamera;
+    public CameraFollow cameraFollow;
     private GameObject otherCharacter;
 
     protected override void Start()
@@ -20,13 +22,29 @@ public class FearlessMovement : CharacterMovement
 
     public override void TurnAround()
     {
+        resetCamera();
         base.TurnAround();
         otherCharacter.GetComponent<CharacterMovement>().Run();
     }
 
     public override void Run()
     {
+        adjustCamera();
         base.Run();
         otherCharacter.GetComponent<CharacterMovement>().StartWalking();
+    }
+
+    private void adjustCamera()
+    {
+        cameraFollow.enabled = false;
+        mainCamera.transform.position = new Vector3(transform.position.x, transform.position.y + 2.9f, transform.position.z + 0.3f);
+        mainCamera.transform.localRotation = Quaternion.Euler(33.56473f, 98.39697f, 5.486476f);
+    }
+
+    private void resetCamera()
+    {
+        cameraFollow.enabled = true;
+        mainCamera.transform.position = new Vector3(cameraFollow.gameObject.transform.position.x, 6.95f, 163.25f);
+        mainCamera.transform.localRotation = Quaternion.Euler(4.587073f, 1.254006f, 0.08177387f);
     }
 }
