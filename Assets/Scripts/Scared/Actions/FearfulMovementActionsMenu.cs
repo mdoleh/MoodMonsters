0cd3cfb68109e6a774278d7b6fa7e3cfe0a2ad7a
Mohammad Doleh <mdoleh@kent.edu>
2015-05-30 13:20:35 -0400

    Successfully reloading scene, correct action broken

---
+++ Assets/Scripts/Scared/Actions/FearfulMovementActionsMenu.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Globals;
+using ScaredScene;
+
+public class FearfulMovementActionsMenu : FearfulMovement
+{
+    protected override void Start()
+    {
+        base.Start();
+        StartCoroutine(ShowActionsMenu());
+    }
+
+    private IEnumerator ShowActionsMenu()
+    {
+        yield return new WaitForSeconds(2f);
+        var actionsCanvas = GameObject.Find("ActionsCanvas");
+        actionsCanvas.GetComponent<Canvas>().enabled = true;
+        Utilities.PlayAudio(actionsCanvas.GetComponent<AudioSource>());
+    }
+}
