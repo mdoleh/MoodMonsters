e415f4cb863ef845979508417d3407c9c74512b2
Mohammad Doleh <mdoleh@kent.edu>
2015-07-06 20:41:22 -0400

    Replaced Scarlet main Scared Scene

--- Assets/Scripts/Scared/Actions/Triggers/TurnAroundTrigger.cs
+++ Assets/Scripts/Scared/Actions/Triggers/TurnAroundTrigger.cs
@@ -1,16 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using ScaredScene;
 
 public class TurnAroundTrigger : MonoBehaviour
 {
     public bool shouldTurnAround = true;
+    private readonly IList<string> characters = new List<string>();
 
     void OnTriggerEnter(Collider other)
     {
-        if (shouldTurnAround)
+        if (shouldTurnAround && !characters.Contains(other.name))
         {
             other.gameObject.GetComponent<CharacterMovement>().TurnAround();
+            characters.Add(other.name);
         }
         else
         {
