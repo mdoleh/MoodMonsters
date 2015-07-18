c02a24ecab37f17e94dce8f149ea7e81a95dc655
Mohammad Doleh <mdoleh@kent.edu>
2015-07-17 23:45:34 -0400

    Cone disappearing

---
+++ Assets/Scripts/Sad/Soccer/ConeDisappear.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Security.Cryptography;
+
+public class ConeDisappear : MonoBehaviour
+{
+    private bool shouldSink = false;
+    private float alpha = 1.0f;
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.transform.name.ToLower().Equals("soccerball"))
+        {
+            StartCoroutine(DestroyObject());
+        }
+    }
+
+    private IEnumerator DestroyObject()
+    {
+        yield return new WaitForSeconds(1f);
+        gameObject.GetComponent<BoxCollider>().enabled = false;
+        yield return new WaitForSeconds(1f);
+        Destroy(gameObject);
+    }
+}
