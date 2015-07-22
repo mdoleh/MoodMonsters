dd4ffd0ec9a5577097137969f6b0d809eacf172c
Mohammad Doleh <mdoleh@kent.edu>
2015-07-22 19:48:01 -0400

    Tantrum option implemented emotionaction

---
+++ Assets/Scripts/Sad/Actions/TemperTantrum.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace SadScene
+{
+    public class TemperTantrum : ActionBase
+    {
+        public AudioSource notFairAudio;
+
+        private Animator anim;
+
+        private void Awake()
+        {
+            anim = GetComponent<Animator>();
+        }
+
+        public override void StartAction()
+        {
+            base.StartAction();
+            StartTantrum();
+        }
+
+        private void StartTantrum()
+        {
+            anim.SetTrigger("Tantrum");
+            StartCoroutine(PlayTantrumAudio());
+        }
+
+        private IEnumerator PlayTantrumAudio()
+        {
+            Utilities.PlayAudio(notFairAudio);
+            yield return new WaitForSeconds(notFairAudio.clip.length);
+            TriggerIncorrect();
+        }
+    }
+}
