0ae7fd9ff91d69dfcfe837a3090b10cd8d954500
Mohammad Doleh <mdoleh@kent.edu>
2015-07-22 22:17:10 -0400

    EmotionActions able to select express sadness answer

---
+++ Assets/Scripts/Sad/Actions/ConveySadness.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConveySadness : ActionBase
+{
+    public AudioSource expressSadDialogue;
+
+    private Animator anim;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public override void StartAction()
+    {
+        base.StartAction();
+        ShowCorrect(true);
+        StartCoroutine(Explain());
+    }
+
+    private IEnumerator Explain()
+    {
+        Utilities.PlayAudio(actionExplanation);
+        yield return new WaitForSeconds(actionExplanation.clip.length);
+        ShowCorrect(false);
+        anim.SetTrigger("Talk");
+        Utilities.PlayAudio(expressSadDialogue);
+        yield return new WaitForSeconds(expressSadDialogue.clip.length);
+        GameObject.Find("EmotionActionsCanvas").GetComponent<Canvas>().enabled = true;
+        GUIDetect.NextGUI();
+    }
+}
