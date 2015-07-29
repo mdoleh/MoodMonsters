37fb07a6edcb461d40aadea550ff19846abffb01
Mohammad Doleh <mdoleh@kent.edu>
2015-07-28 20:05:55 -0400

    Fixed grass on terrain

--- Assets/Scripts/Emotions/Sad/Soccer/OutsideGroupSoccerAnimation.cs
+++ Assets/Scripts/Emotions/Sad/Soccer/OutsideGroupSoccerAnimation.cs
@@ -93,7 +93,7 @@ namespace SadScene
         private IEnumerator KickBallForward()
         {
             anim.SetTrigger("KickForward");
-            yield return new WaitForSeconds(1f);
+            yield return new WaitForSeconds(2f);
             GetComponent<CapsuleCollider>().enabled = true;
         }
 
