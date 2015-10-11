11736c720007b30a14acd3ab1b88f21219006683
Mohammad Doleh <mdoleh@kent.edu>
2015-10-11 16:33:27 -0400

    Removed timing mechanism for moving ipad

--- Assets/Scripts/Emotions/Angry/Sequence/AnimationActions.cs
+++ Assets/Scripts/Emotions/Angry/Sequence/AnimationActions.cs
@@ -29,6 +29,20 @@ namespace AngryScene
         {
             anim.SetTrigger("IsAngry");
         }
+
+        public void MoveIpadUnderArm()
+        {
+            var ipad = GameObject.Find("iPad");
+            ipad.transform.localPosition = new Vector3(0.04f, -0.073f, 0.049f);
+            ipad.transform.localRotation = Quaternion.Euler(352.4399f, 357.2535f, 10.56083f);
+        }
+
+        public void MoveIpadToLap()
+        {
+            var ipad = GameObject.Find("iPad");
+            ipad.transform.localPosition = new Vector3(0.224f, 0.145f, 0.043f);
+            ipad.transform.localRotation = Quaternion.Euler(15.41914f, 158.8406f, 1.443392f);
+        }
     }
 }
 
