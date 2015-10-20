4573b42801ffcbbaf0d8d7d5b727155d640e824e
Mohammad Doleh <mdoleh@kent.edu>
2015-10-20 19:28:24 -0400

    New dragging animation script

---
+++ Assets/Scripts/Utilities/Animations/DraggingHorizontally.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class DraggingHorizontally : MonoBehaviour
+{
+    public Transform dragging;
+    public Transform target;
+
+    public bool moveTargetWithDragging = false;
+
+    public float speed;
+    // this is speedSmallScreen / (screenWidth / speedSmallScreen)
+    // test on small screen first, determine desired speed, then 
+    // compute constant using above
+    public float constant;
+
+    private Vector3 initialPosition;
+
+    private void Start()
+    {
+        initialPosition = dragging.localPosition;
+    }
+
+    private void Update()
+    {
+        dragging.localPosition = new Vector3(dragging.localPosition.x + Time.deltaTime * (Screen.width / speed) * constant,
+                dragging.localPosition.y);
+        if (dragging.localPosition.x >= target.localPosition.x / 2.0f)
+        {
+            dragging.localPosition = initialPosition;
+        }
+
+        if (moveTargetWithDragging) target.localPosition = new Vector2(dragging.localPosition.x, target.localPosition.y);
+    }
+}
