82241223b4de75eea5bef4873312ffd0526ff83a
Mohammad Doleh <mdoleh@kent.edu>
2015-11-19 18:19:45 -0500

    SoccerMovement snaps on swipe and sticks
    allows movement again when joystick is released

--- Assets/Scripts/Emotions/Sad/Soccer/SoccerMovementHandler.cs
+++ Assets/Scripts/Emotions/Sad/Soccer/SoccerMovementHandler.cs
@@ -13,24 +13,26 @@ public class SoccerMovementHandler : MovementHandler
     {
         float moveSpeed = Time.deltaTime * joystick.CurrentSpeedAndDirection.y;
         transform.position = new Vector3(transform.position.x + moveSpeed, transform.position.y, transform.position.z);
-        StartCoroutine(adjustPosition(transform, joystick));
+        adjustPosition(transform, joystick);
     }
 
-    private IEnumerator adjustPosition(Transform transform, Joystick joystick)
+    private void adjustPosition(Transform transform, Joystick joystick)
     {
-        if (!shouldAdjustPosition) yield break;
-        shouldAdjustPosition = false;
-        yield return new WaitForSeconds(0.5f);
+        if (joystick.CurrentSpeedAndDirection.x == 0f) shouldAdjustPosition = true;
+        if (!shouldAdjustPosition) return;
         float newPositionZ = transform.position.z;
-        if (joystick.CurrentSpeedAndDirection.x < -0.5f)
+        if (joystick.CurrentSpeedAndDirection.x <= -1.0f)
         {
             newPositionZ = currentIndex == 0 ? lanes[currentIndex].position.z : lanes[--currentIndex].position.z;
+            shouldAdjustPosition = false;
         }
-        else if (joystick.CurrentSpeedAndDirection.x > 0.5f)
+        else if (joystick.CurrentSpeedAndDirection.x >= 1.0f)
         {
-            newPositionZ = currentIndex == lanes.Length - 1 ? lanes[currentIndex].position.z : lanes[++currentIndex].position.z;
+            newPositionZ = currentIndex == lanes.Length - 1
+                ? lanes[currentIndex].position.z
+                : lanes[++currentIndex].position.z;
+            shouldAdjustPosition = false;
         }
         transform.position = new Vector3(transform.position.x, transform.position.y, newPositionZ);
-        shouldAdjustPosition = true;
     }
 }
