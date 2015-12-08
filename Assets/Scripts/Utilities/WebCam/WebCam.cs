df48bb8a12ddada060c4dc0ad633e018177b9274
Mohammad Doleh <mdoleh@kent.edu>
2015-12-07 19:19:11 -0500

    Fixed camera orientation detection
    Image only flips for landscape left and right

--- Assets/Scripts/Utilities/WebCam/WebCam.cs
+++ Assets/Scripts/Utilities/WebCam/WebCam.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using UnityEngine.iOS;
 using UnityEngine.UI;
 
 public class WebCam : MonoBehaviour
@@ -28,10 +29,24 @@ public class WebCam : MonoBehaviour
                 GetComponent<RawImage>().transform.localScale = new Vector3(-1, 1);
         }
         // this assumes the game is only allowed in landscape mode
-        // testing shows default is "FaceUp", and flipping produces "Unknown"
+        var opposite = determineOrientation();
+        if (opposite == DeviceOrientation.Unknown) return;
         GetComponent<RawImage>().transform.localRotation = 
-            Quaternion.Euler(defaultOrientation == Input.deviceOrientation ? 
+            Quaternion.Euler(Input.deviceOrientation == opposite ? 
             new Vector3(originalRotation.x, originalRotation.y, 180f) : originalRotation);
+    }
+
+    private DeviceOrientation determineOrientation()
+    {
+        if (defaultOrientation == DeviceOrientation.LandscapeLeft)
+        {
+            return DeviceOrientation.LandscapeRight;
+        }
+        if (defaultOrientation == DeviceOrientation.LandscapeRight)
+        {
+            return DeviceOrientation.LandscapeLeft;
+        }
+        return DeviceOrientation.Unknown;
     }
 
     public void TurnOnCamera()
