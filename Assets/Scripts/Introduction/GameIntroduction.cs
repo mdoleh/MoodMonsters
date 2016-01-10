55fb3307604d0b803e54a7cc92a13fc2b5cc5dba
Mohammad Doleh <mdoleh@kent.edu>
2016-01-10 15:33:50 -0500

    Beginnings of Game Intro

---
+++ Assets/Scripts/Introduction/GameIntroduction.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class GameIntroduction : MonoBehaviour
+{
+    public BasicCameraMovements mainCamera;
+
+    [Header("Kids' Audio")]
+    // Kids' audio
+    public AudioSource bodyAndFace;
+    public AudioSource useWordsLike;
+    public AudioSource[] emotions;
+
+    [Header("Adults' Audio")]
+    // Adults' audio
+    public AudioSource PASSIntro;
+    public AudioSource[] PASSLetters;
+
+    private void Start()
+    {
+        StartCoroutine(playIntro());
+    }
+
+    private IEnumerator playIntro()
+    {
+        yield return new WaitForSeconds(2f);
+        mainCamera.PanRight((position) => position.x <= 204.352f);
+        Utilities.PlayAudio(bodyAndFace);
+        yield return new WaitForSeconds(bodyAndFace.clip.length);
+        Utilities.PlayAudio(useWordsLike);
+        yield return new WaitForSeconds(useWordsLike.clip.length);
+        yield return StartCoroutine(playEmotions());
+    }
+
+    private IEnumerator playEmotions()
+    {
+        foreach (var emotion in emotions)
+        {
+            emotion.gameObject.SetActive(true);
+            Utilities.PlayAudio(emotion);
+            yield return new WaitForSeconds(emotion.clip.length);
+        }
+    }
+}
---
+++ Assets/Scripts/Utilities/BasicCameraMovements.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using UnityEngine;
+
+public class BasicCameraMovements : MonoBehaviour
+{
+    public float PAN_SPEED = 0.01f;
+
+    private bool panUp;
+    private bool panDown;
+    private bool panLeft;
+    private bool panRight;
+
+    private Func<Vector3, bool> conditionUp;
+    private Func<Vector3, bool> conditionDown;
+    private Func<Vector3, bool> conditionLeft;
+    private Func<Vector3, bool> conditionRight; 
+
+    public void PanLeft(Func<Vector3, bool> condition)
+    {
+        panLeft = true;
+        conditionLeft = condition;
+    }
+
+    public void PanRight(Func<Vector3, bool> condition)
+    {
+        panRight = true;
+        conditionRight = condition;
+    }
+
+    public void PanUp(Func<Vector3, bool> condition)
+    {
+        panUp = true;
+        conditionUp = condition;
+    }
+
+    public void PanDown(Func<Vector3, bool> condition)
+    {
+        panDown = true;
+        conditionDown = condition;
+    }
+
+    public void ResetFlags()
+    {
+        panUp = false;
+        panDown = false;
+        panLeft = false;
+        panRight = false;
+
+        conditionUp = null;
+        conditionDown = null;
+        conditionLeft = null;
+        conditionRight = null; 
+    }
+
+    private void Update()
+    {
+        if (panUp)
+        {
+            transform.position += transform.up * PAN_SPEED;
+            if (conditionUp(transform.position)) panUp = false;
+        }
+        if (panDown)
+        {
+            transform.position -= transform.up * PAN_SPEED;
+            if (conditionDown(transform.position)) panDown = false;
+        }
+        if (panLeft)
+        {
+            transform.position -= transform.right * PAN_SPEED;
+            if (conditionLeft(transform.position)) panLeft = false;
+        }
+        if (panRight)
+        {
+            transform.position += transform.right * PAN_SPEED;
+            if (conditionRight(transform.position)) panRight = false;
+        }
+    }
+}
