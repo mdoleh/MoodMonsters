be33bedb5761f80d246947eb054d960c11eb9c12
Mohammad Doleh <mdoleh@kent.edu>
2016-02-07 11:00:35 -0500

    Angry stomp animation
    still needs to be resized

---
+++ Assets/Scripts/Introduction/AngryAnimation.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace GameIntro
+{
+    public class AngryAnimation : MonoBehaviour
+    {
+        private Animator anim;
+
+        private void Start()
+        {
+            anim = GetComponent<Animator>();
+        }
+
+        public void ShiftAngry()
+        {
+            anim.SetTrigger("Shift");
+        }
+    }
+}
