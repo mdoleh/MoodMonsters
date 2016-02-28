0b96352f31220920701d92b955ee7398aa4b88f2
Mohammad Doleh <mdoleh@kent.edu>
2016-02-28 15:18:52 -0500

    PASS reminder on + angry stomp slowed down

--- Assets/Scripts/Globals/GameFlags.cs
+++ Assets/Scripts/Globals/GameFlags.cs
@@ -7,7 +7,7 @@
         public static bool PuzzleTutorialHasRun = true;
         public static bool CameraTutorialHasRun = true;
         public static bool JoyStickTutorialHasRun = true;
-        public static bool HasSeenPASS = false;
+        public static bool HasSeenPASS = true;
         public static bool AdultIsPresent = true;
         public static string ParentGender = "Mom";
 
@@ -18,7 +18,7 @@
             BucketTutorialHasRun = false;
             PuzzleTutorialHasRun = false;
             CameraTutorialHasRun = false;
-            HasSeenPASS = false;
+            HasSeenPASS = true;
             AdultIsPresent = false;
             ParentGender = "";
         }
