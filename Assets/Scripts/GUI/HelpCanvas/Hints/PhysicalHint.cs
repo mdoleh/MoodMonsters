802826831eea20706fc31fbfb1b81d7300e5a11a
Mohammad Doleh <mdoleh@kent.edu>
2016-03-14 22:22:28 -0400

    High five hint working

--- Assets/Scripts/GUI/HelpCanvas/Hints/PhysicalHint.cs
+++ Assets/Scripts/GUI/HelpCanvas/Hints/PhysicalHint.cs
@@ -3,19 +3,20 @@ using UnityEngine;
 
 public class PhysicalHint : SimpleHint
 {
-    public Animator anim;
+    public Animator[] anims;
     public string animationTrigger;
     public string animationResetTrigger;
 
     public override void ShowHint()
     {
         base.ShowHint();
-        if (!anim.GetCurrentAnimatorStateInfo(0).IsName(animationTrigger))
-            anim.SetTrigger(animationTrigger);
+        if (!anims.ToList().TrueForAll(x => x.GetCurrentAnimatorStateInfo(0).IsName(animationTrigger)))
+            anims.ToList().ForEach(x => x.SetTrigger(animationTrigger));
     }
 
     public override void NotifyCanvasChange()
     {
-        anim.SetTrigger(animationResetTrigger);
+        if (anims.ToList().TrueForAll(x => x.GetCurrentAnimatorStateInfo(0).IsName(animationTrigger)))
+            anims.ToList().ForEach(x => x.SetTrigger(animationResetTrigger));
     }
 }
