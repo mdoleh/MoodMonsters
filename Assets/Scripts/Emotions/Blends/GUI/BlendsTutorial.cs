1830303595f3b9044254e9fafc2691dc17892fa8
Mohammad Doleh <mdoleh@kent.edu>
2016-05-21 14:53:04 -0400

    Emotion blends scene able to start, character gender based on player, parent gender chosen at random

--- Assets/Scripts/Emotions/Blends/GUI/BlendsTutorial.cs
+++ Assets/Scripts/Emotions/Blends/GUI/BlendsTutorial.cs
@@ -8,13 +8,19 @@ namespace BlendsScene
     {
         public GameObject[] childrenPairs;
         public GameObject[] parents;
+        public AudioSource[] introAudioClips;
 
         public override void InitializeGameObjects()
         {
-            base.InitializeGameObjects();
             GameFlags.ParentGender = Random.Range(0, 2) == 0 ? "Mom" : "Dad";
-            childrenPairs.ToList().Find(x => x.name.ToLower().Equals(GameFlags.PlayerGender.ToLower())).SetActive(true);
-            parents.ToList().Find(x => x.name.ToLower().Equals(GameFlags.ParentGender)).SetActive(true);
+            childrenPairs.ToList().Find(x => x.name.ToLower().Contains(GameFlags.PlayerGender.ToLower())).SetActive(true);
+            parents.ToList().Find(x => x.name.ToLower().Contains(GameFlags.ParentGender.ToLower())).SetActive(true);
+        }
+
+        protected override void InitializeAudio()
+        {
+            introAudio = introAudioClips.ToList()
+                .Find(x => x.gameObject.name.ToLower().Contains(GameFlags.PlayerGender.ToLower()));
         }
     }
 }
