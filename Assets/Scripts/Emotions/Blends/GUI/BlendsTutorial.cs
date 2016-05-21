fd9d43ca5af58f8f4124192afbf17a5715922cca
Mohammad Doleh <mdoleh@kent.edu>
2016-05-21 17:04:22 -0400

    EmotionBlends scene sucessfully initializes and starts the first piece of the scene, characters are introduced and parent walks into frame

--- Assets/Scripts/Emotions/Blends/GUI/BlendsTutorial.cs
+++ Assets/Scripts/Emotions/Blends/GUI/BlendsTutorial.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections;
+using System.Linq;
 using Globals;
 using UnityEngine;
 
@@ -9,18 +10,40 @@ namespace BlendsScene
         public GameObject[] childrenPairs;
         public GameObject[] parents;
         public AudioSource[] introAudioClips;
+        public AudioSource[] hereComesParent;
+
+        private GameObject currentParent;
 
         public override void InitializeGameObjects()
         {
+            helpCanvas = GameObject.Find("HelpCanvas");
+            disablePanel = helpCanvas.transform.FindChild("DisablePanel").gameObject;
+            noInputSymbol = disablePanel.transform.FindChild("NoInputSymbol").gameObject;
+
             GameFlags.ParentGender = Random.Range(0, 2) == 0 ? "Mom" : "Dad";
             childrenPairs.ToList().Find(x => x.name.ToLower().Contains(GameFlags.PlayerGender.ToLower())).SetActive(true);
-            parents.ToList().Find(x => x.name.ToLower().Contains(GameFlags.ParentGender.ToLower())).SetActive(true);
+            currentParent = parents.ToList().Find(x => x.name.ToLower().Contains(GameFlags.ParentGender.ToLower()));
         }
 
         protected override void InitializeAudio()
         {
             introAudio = introAudioClips.ToList()
                 .Find(x => x.gameObject.name.ToLower().Contains(GameFlags.PlayerGender.ToLower()));
+        }
+
+        protected override void HelpExplanationComplete()
+        {
+            base.HelpExplanationComplete();
+            StartCoroutine(playHereComesParent());
+        }
+
+        private IEnumerator playHereComesParent()
+        {
+            var audioToPlay =
+                hereComesParent.ToList().Find(x => x.name.ToLower().Contains(GameFlags.ParentGender.ToLower()));
+            Utilities.PlayAudio(audioToPlay);
+            yield return new WaitForSeconds(audioToPlay.clip.length);
+            currentParent.SetActive(true);
         }
     }
 }
---
+++ Assets/Scripts/Emotions/Blends/Sequence/WalkForward.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace BlendsScene
+{
+    public class WalkForward : MonoBehaviour
+    {
+        private Animator anim;
+
+        private void Start()
+        {
+            anim = GetComponent<Animator>();
+        }
+
+        private void Update()
+        {
+            if (anim.GetBool("Walk"))
+            {
+                changePosition(1f);
+                if (transform.position.z <= 143.637f) anim.SetBool("Walk", false);
+            }
+        }
+
+        private void changePosition(float speed)
+        {
+            float move = Time.deltaTime * speed;
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - move);
+        }
+    }
+}
